bf666e123522c6ed29ae34cf7bf2ca1ab0bab165
Anatoliy Roslyakov <91376345+AnatoliyRoslyakov@users.noreply.github.com>
2024-08-30 02:49:54 +0300

    package assets

--- packages/calendar/lib/src/theme/svgs.dart
+++ packages/calendar/lib/src/theme/svgs.dart
@@ -2,8 +2,6 @@ typedef SvgAsset = String;
 
 class Svgs {
   Svgs._();
-  static const SvgAsset left =
-      '/Users/anatolijroslakov/GitHub/teacher_mate/packages/calendar/assets/svg/angle-left.svg';
-  static const SvgAsset right =
-      '/Users/anatolijroslakov/GitHub/teacher_mate/packages/calendar/assets/svg/angle-right.svg';
+  static const SvgAsset left = 'packages/calendar/assets/svg/angle-left.svg';
+  static const SvgAsset right = 'packages/calendar/assets/svg/angle-right.svg';
 }
